f05f1db1de193d687cdf9f93f89c6934f1a7aad6
amirafarag <amiraazam301@gmail.com>
2024-04-25 11:16:59 +0200

    revision

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+// 1-Storing the color of a chosen product (e.g., "red", "blue", "green")
+
+var colorOfChosen = 'red, blue, green';
+console.log(colorOfChosen, typeof(colorOfChosen));
+
+// 2-Representing if a user has subscribed to a newsletter
+
+var subscribedUser = true;
+console.log(subscribedUser, typeof (subscribedUser));
+
+// 3-Calculating the area of a rectangle based on its width and height
+
+var calculatingTheArea = '30cm';
+console.log(calculatingTheArea, typeof (calculatingTheArea));
+
+// 4-Displaying a welcome message to the user
+
+var message = 'Welcome';
+console.log(message, typeof (message));
+
+// 5-Checking if a password meets certain length requirements (using a number) - Number and String combination
+
+var passwordMeets = true;
+console.log(passwordMeets, typeof(passwordMeets));
+
+// 6-Keeping track of the number of lives remaining in a game
+
+var livesRemaining = 4;
+console.log(livesRemaining, typeof(livesRemaining));
